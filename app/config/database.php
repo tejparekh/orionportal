34c03dfec6811565c4717ec3bee349c3d0da43aa
unknown <tejparekh2402@gmail.com>
2015-08-22 22:31:23 +0530

    config dDB

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'oriondb_ufo',
+			'database'  => 'oriondb',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => '0rion@123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
